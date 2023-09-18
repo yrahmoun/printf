fff08ba70ec93846c4c55f8879f123cd41fe2e51
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-09-18 11:25:40 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -33,8 +33,7 @@ int _printf(const char *format, ...)
 	int counter = 0;
 	int i = 0;
 
-	if (!format || (format[0] == '%' && !format[1])
-			|| format[0])
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	va_start(list, format);
 	while (format[i])
